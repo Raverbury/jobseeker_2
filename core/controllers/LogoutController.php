e057f42861aea55088694a261fdfeb60bda38a60
Raverbury <smurfjp198@gmail.com>
2022-04-02 20:52:40 +0700

    Added action switch

--- core/controllers/LogoutController.php
+++ core/controllers/LogoutController.php
@@ -2,13 +2,21 @@
 
 class LogoutController extends Controller {
   public function process($params) {
-		if ($_SESSION['isLoggedIn']) {
-			session_unset();
-			$_SESSION['username'] = 'Guest';
-			$_SESSION['id'] = -1;
-			$_SESSION['role'] = 'guest';
-			$_SESSION['isLoggedIn'] = false;
-		}
-		$this->redirect('home');
+		$action = array_shift($params);
+		switch ($action) {
+			case '':
+				if ($_SESSION['isLoggedIn']) {
+					session_unset();
+					$_SESSION['username'] = 'Guest';
+					$_SESSION['id'] = -1;
+					$_SESSION['role'] = 'guest';
+					$_SESSION['isLoggedIn'] = false;
+				}
+				$this->redirect('home');
+				break;
+			default:
+				$this->redirect('error');
+				break;
+		}		
   }
 }
--- core/controllers/RegisterController.php
+++ core/controllers/RegisterController.php
@@ -1,33 +1,41 @@
 <?php
 
-class RegisterController extends Controller {
-  public function process($params) {
-		// if this is from a register attempt aka with $_POST
-		if ($_SERVER["REQUEST_METHOD"] == "POST") {
-			require("../core/models/RegisterModel.php");
-			$registerModel = new RegisterModel();
-			$registerModel->executeQuery($_POST);
-			$result = $registerModel->getResult();
-			if ($result['message'] == 'OK') {
-				$_SESSION['username'] = $result['username'];
-				$_SESSION['id'] = $result['id'];
-				$_SESSION['role'] = $result['role'];
-				$_SESSION['isLoggedIn'] = true;
-				$this->redirect('home');
-			}
-			else {
-				$this->data['message'] = $result['message'];
-				$this->view = 'register';
-			}
+class RegisterController extends Controller
+{
+	public function process($params)
+	{
+		$action = array_shift($params);
+		switch ($action) {
+			case '':
+				// if this is from a register attempt aka with $_POST
+				if ($_SERVER["REQUEST_METHOD"] == "POST") {
+					require("../core/models/RegisterModel.php");
+					$registerModel = new RegisterModel();
+					$registerModel->executeQuery($_POST);
+					$result = $registerModel->getResult();
+					if ($result['message'] == 'OK') {
+						$_SESSION['username'] = $result['username'];
+						$_SESSION['id'] = $result['id'];
+						$_SESSION['role'] = $result['role'];
+						$_SESSION['isLoggedIn'] = true;
+						$this->redirect('home');
+					} else {
+						$this->data['message'] = $result['message'];
+						$this->view = 'register';
+					}
+				}
+				// without $_POST aka first time viewing the page
+				else {
+					if ($_SESSION['isLoggedIn']) {
+						$this->redirect('home');
+					} else {
+						$this->view = 'register';
+					}
+				}
+				break;
+			default:
+				$this->redirect('error');
+				break;
 		}
-		// without $_POST aka first time viewing the page
-		else {
-			if ($_SESSION['isLoggedIn']) {
-				$this->redirect('home');
-			}
-			else {
-				$this->view = 'register';
-			}
-		}
-  }
+	}
 }
