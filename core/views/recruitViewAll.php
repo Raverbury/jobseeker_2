bfa3622e51080accc0b1088b5866be115fb38738
Raverbury <smurfjp198@gmail.com>
2022-06-02 19:14:45 +0700

    Updated UI

--- core/views/recruitViewAll.php
+++ core/views/recruitViewAll.php
@@ -1,17 +1,27 @@
 <div class="container">
-  <h1 class="py-2">Your Job Posts</h2>
-    <?php
-    $records = count($this->result['data'][0]); //number of job posts
-    $data = $this->result['data'];
-    for ($i = 0; $i < $records; $i++) {
-      echo '
-            <a href="/recruit/getId/' . $data[0][$i] . '">
-            <h1>' . $data[2][$i] . '</h1>
-            <h2>Title: ' . $data[3][$i] . '</h2>
-            <h2>Required ' . $data[4][$i] . ' year(s) of experience</h2>
-            </a>
-            ';
-    }
-    ?>
-    <a href="/recruit/create" class="btn btn-primary">Create Post</a>
+  <h1 class="py-2">All job descriptions</h2>
+  <p>This page lists all JDs.</p>
+  <table class="table table-responsive-md table-bordered table-striped">
+    <thead class="table-dark">
+      <tr>
+        <th scope="col">Company</th>
+        <th scope="col">Title</th>
+        <th scope="col">Minimum experience</th>
+        <th scope="col">View</th>
+      </tr>
+    </thead>
+    <tbody>
+      <?php
+      for ($i = 0; $i < count($this->result['data'][0]); $i++) {
+        echo '<tr>
+          <td>' . $this->result['data'][2][$i] . '</td>
+          <td>' . $this->result['data'][3][$i] . '</td>
+          <td>' . $this->result['data'][4][$i] . '</td>
+          <td><a href="recruit/view/' . $this->result['data'][0][$i] . '"><u>View</u></a></td>
+        </tr>';
+      }
+      ?>
+    </tbody>
+  </table>
+  <a href="/recruit/create" class="btn btn-primary m-1">Create Post</a>
 </div>
