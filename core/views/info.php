c509b1f175e203f36f2af1d214e1feda89638eba
Raverbury <smurfjp198@gmail.com>
2022-06-10 09:47:33 +0700

    Updated info page

--- core/views/info.php
+++ core/views/info.php
@@ -1,19 +1,9 @@
-<!-- <div class="container">
-  <h1 class="py-2">ABOUT US</h1>
-  <p>This section contains all available boostrap buttons.</p>
-  <div class="container p-2" style="background-color:rgb(196, 173, 173)">
-    <button class="btn btn-primary">Primary</button>
-    <button class="btn btn-secondary">Secondary</button>
-    <button class="btn btn-success">Success</button>
-    <button class="btn btn-info">Info</button>
-    <button class="btn btn-warning">Warning</button>
-    <button class="btn btn-danger">Danger</button>
-    <button class="btn btn-light">Light</button>
-    <button class="btn btn-dark">Dark</button>
+<div class="container">
+  <h1 class="py-2">Sitemap</h1>
+  <p>This section contains information regarding site navigation.</p>
+  <div class="container">
+    <img src="/Template/123.png" alt="The sitemap for this website" title="Sitemap">
   </div>
   Tip: <i>most if not all of these buttons do nothing.</i>
-</div> -->
-
-<div class="container">
-  <img src="/Template/123.png">
 </div>
+
